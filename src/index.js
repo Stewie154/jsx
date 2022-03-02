e3958e21fa1bf22a52e9b87db3a050be6c968618
Stewart McFarlane <stewart@toru.digital>
2022-03-02 16:11:10 +0000

    added content

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ const App = () => {
 	const buttonText = {text: 'Click me!'}
 	const labelText = 'Enter Name'
     return (
-        <div>
-			Hi There
+        <div className='ui container comments'>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img className="avatar"/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00pm</span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
 		</div>
     )
 }
