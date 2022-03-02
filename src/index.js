460f7253173088c4eb89cd2b038cdcc7fea63448
Stewart McFarlane <stewart@toru.digital>
2022-03-02 15:24:56 +0000

    added jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM  from 'react-dom'
 
 const App = () => {
     return (
-        <div>Hello React</div>
+        <div>
+			<label className="label" for="name">Enter Name:</label>
+			<input id="name" type="text" />
+			<button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+		</div>
     )
 }
 
