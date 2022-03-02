33cc2fa3b4df1b80b5a0bbc884574e15821bdce0
Stewart McFarlane <stewart@toru.digital>
2022-03-02 15:30:52 +0000

    button text with function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 
 const App = () => {
+	const getButtonText = () => {
+		return 'Click on me!'
+	}
     return (
         <div>
 			<label className="label" for="name">Enter Name:</label>
 			<input id="name" type="text" />
-			<button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+			<button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
 		</div>
     )
 }
